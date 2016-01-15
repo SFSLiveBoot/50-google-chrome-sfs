4315fd4799339cc2e772f9468ca7db3ffc0be8d0
Lauri Korts-Pärn <lauri@korc.jp>
2016-01-14 20:10:15 -0500

    use versioned filename in download

---
+++ usr/src/sfs.d/.common.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+: ${repo:=https://dl.google.com/linux/chrome/deb}
+: ${package_name:=google-chrome-stable}
+: ${packages_url:=$repo/dists/stable/main/binary-amd64/Packages.gz}
+
+latest_url() {
+  echo "$repo/$(curl -s "$packages_url" | gzip -dc | grep -e ^Package: -e ^Filename: | grep -FxA1 "Package: $package_name" | awk '/^Filename: /{print $2}')"
+}
