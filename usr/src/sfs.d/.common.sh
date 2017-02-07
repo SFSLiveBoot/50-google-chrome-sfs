e7ff57bd1aa06a30af018de7b8d77951b83f9f14
Lauri Korts-Pärn <lauri@korc.jp>
2017-02-06 20:54:37 -0500

    add latest version check script

--- usr/src/sfs.d/.common.sh
+++ usr/src/sfs.d/.common.sh
@@ -7,6 +7,20 @@
 : ${package_name:=google-chrome-stable}
 : ${packages_url:=$repo/dists/stable/main/binary-amd64/Packages.gz}
 
+
+extract_deb_field() {
+  local url="$1" package="$2" field="$3"
+  curl -s "$url" | gzip -dc | grep -e ^Package: -e "^$field:" | grep -FxA1 "Package: $package" | awk "/^$field: /{print \$2}"
+}
+
 latest_url() {
-  echo "$repo/$(curl -s "$packages_url" | gzip -dc | grep -e ^Package: -e ^Filename: | grep -FxA1 "Package: $package_name" | awk '/^Filename: /{print $2}')"
+  echo "$repo/$(extract_deb_field "$packages_url" "$package_name" "Filename")"
+}
+
+latest_version() {
+  extract_deb_field "$packages_url" "$package_name" "Version"
+}
+
+current_version() {
+  "$DESTDIR/opt/google/chrome/chrome" --version | grep -Eo '[1-9][0-9.]+'
 }
